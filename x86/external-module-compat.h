d2783d1b250762873fd9e740ce813809200f3ff4
Jan Kiszka <jan.kiszka@siemens.com>
2010-09-29 15:10:51 +0200

    Remove more pre-2.6.23 compat stuff
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- x86/external-module-compat.h
+++ x86/external-module-compat.h
@@ -313,58 +313,6 @@ static inline int rdmsrl_safe(unsigned msr, unsigned long long *p)
 #undef X86_CR8_TPR
 #define X86_CR8_TPR 0x0f
 
-/*
- * 2.6.22 does not define set_64bit() under nonpae
- */
-#ifdef CONFIG_X86_32
-
-#include <asm/cmpxchg.h>
-
-static inline void __kvm_set_64bit(u64 *ptr, u64 val)
-{
-	unsigned int low = val;
-	unsigned int high = val >> 32;
-
-	__asm__ __volatile__ (
-		"\n1:\t"
-		"movl (%0), %%eax\n\t"
-		"movl 4(%0), %%edx\n\t"
-		"lock cmpxchg8b (%0)\n\t"
-		"jnz 1b"
-		: /* no outputs */
-		:	"D"(ptr),
-			"b"(low),
-			"c"(high)
-		:	"ax","dx","memory");
-}
-
-#undef  set_64bit
-#define set_64bit __kvm_set_64bit
-
-static inline unsigned long long __kvm_cmpxchg64(volatile void *ptr,
-						 unsigned long long old,
-						 unsigned long long new)
-{
-	unsigned long long prev;
-	__asm__ __volatile__("lock cmpxchg8b %3"
-			     : "=A"(prev)
-			     : "b"((unsigned long)new),
-			       "c"((unsigned long)(new >> 32)),
-			       "m"(*__xg(ptr)),
-			       "0"(old)
-			     : "memory");
-	return prev;
-}
-
-#define kvm_cmpxchg64(ptr,o,n)\
-	((__typeof__(*(ptr)))__kvm_cmpxchg64((ptr),(unsigned long long)(o),\
-					(unsigned long long)(n)))
-
-#undef cmpxchg64
-#define cmpxchg64(ptr, o, n) kvm_cmpxchg64(ptr, o, n)
-
-#endif
-
 #ifndef CONFIG_PREEMPT_NOTIFIERS
 
 #ifdef CONFIG_HAVE_HW_BREAKPOINT
