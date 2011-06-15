8d0eae67e15b800e17e51fa9e506e1fc2d76ea1a
Jan Kiszka <jan.kiszka@siemens.com>
2011-06-15 22:15:52 +0200

    Provide sigset_from_compat for kernels < 3.1
    The function is not exported on older kernels.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- external-module-compat-comm.h
+++ external-module-compat-comm.h
@@ -964,3 +964,21 @@ static inline void rcu_virt_note_context_switch(int cpu)
 {
 }
 #endif
+
+#ifdef CONFIG_COMPAT
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,1,0)
+#include <linux/compat.h>
+static inline
+void kvm_sigset_from_compat(sigset_t *set, compat_sigset_t *compat)
+{
+	switch (_NSIG_WORDS) {
+	case 4: set->sig[3] = compat->sig[6] | (((long)compat->sig[7]) << 32 );
+	case 3: set->sig[2] = compat->sig[4] | (((long)compat->sig[5]) << 32 );
+	case 2: set->sig[1] = compat->sig[2] | (((long)compat->sig[3]) << 32 );
+	case 1: set->sig[0] = compat->sig[0] | (((long)compat->sig[1]) << 32 );
+	}
+}
+#else /* >= 3.1 */
+#define kvm_sigset_from_compat	sigset_from_compat
+#endif /* >= 3.1 */
+#endif /* CONFIG_COMPAT */
