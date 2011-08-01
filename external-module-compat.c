68c00ccfa8bcda23ecfdda191d012783dfe82ec2
Jan Kiszka <jan.kiszka@siemens.com>
2011-08-01 13:51:59 +0200

    Add bsearch header and body for kernels < 3.0
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- external-module-compat.c
+++ external-module-compat.c
@@ -334,3 +334,27 @@ void unregister_syscore_ops(struct syscore_ops *ops)
 EXPORT_SYMBOL_GPL(unregister_syscore_ops);
 
 #endif /* < 2.6.39 */
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,0,0)
+void *bsearch(const void *key, const void *base, size_t num, size_t size,
+	      int (*cmp)(const void *key, const void *elt))
+{
+	size_t start = 0, end = num;
+	int result;
+
+	while (start < end) {
+		size_t mid = start + (end - start) / 2;
+
+		result = cmp(key, base + mid * size);
+		if (result < 0)
+			end = mid;
+		else if (result > 0)
+			start = mid + 1;
+		else
+			return (void *)base + mid * size;
+	}
+
+	return NULL;
+}
+EXPORT_SYMBOL(bsearch);
+#endif /* < 3.0 */
