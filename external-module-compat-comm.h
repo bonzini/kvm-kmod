5dd3181a976370eb17ea2ab549bb6fa6f4dd7048
Paolo Bonzini <pbonzini@redhat.com>
2017-03-27 17:57:26 +0200

    remove device assignment support
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- external-module-compat-comm.h
+++ external-module-compat-comm.h
@@ -801,49 +801,6 @@ perf_unregister_guest_info_callbacks(struct perf_guest_info_callbacks *cbs)
 #define lockdep_is_held(m)		(1)
 #endif
 
-#ifdef CONFIG_IOMMU_API
-#include <linux/iommu.h>
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,3,0)
-
-#define kvm_iommu_map	iommu_map
-#define kvm_iommu_unmap	iommu_unmap
-
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,35)
-
-static inline int kvm_iommu_map(struct iommu_domain *domain,
-				unsigned long iova, phys_addr_t paddr,
-				size_t size, int prot)
-{
-	return iommu_map(domain, iova, paddr, get_order(size), prot);
-}
-
-static inline int kvm_iommu_unmap(struct iommu_domain *domain,
-				  unsigned long iova, size_t size)
-{
-	return PAGE_SIZE << iommu_unmap(domain, iova, get_order(size));
-}
-
-#else /* < 2.6.35 */
-
-static inline int kvm_iommu_map(struct iommu_domain *domain,
-				unsigned long iova, phys_addr_t paddr,
-				size_t size, int prot)
-{
-	return iommu_map_range(domain, iova, paddr, size, prot);
-}
-
-static inline int kvm_iommu_unmap(struct iommu_domain *domain,
-				  unsigned long iova, size_t size)
-{
-	iommu_unmap_range(domain, iova, size);
-
-	return size;
-}
-#endif /* < 2.6.35 */
-
-#endif /* CONFIG_IOMMU_API */
-
 #ifndef lower_32_bits
 #define lower_32_bits(n) ((u32)(n))
 #endif
