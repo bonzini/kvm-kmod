0a17cfc00f60082793448b07036f956055d7db7b
Paolo Bonzini <pbonzini@redhat.com>
2018-03-15 22:56:05 +0100

    4.17

--- x86/compat-x86.c
+++ x86/compat-x86.c
@@ -1,1 +1,12 @@
-/* empty */
+#include <linux/types.h>
+#include <linux/module.h>
+
+#if IS_ENABLED(CONFIG_HYPERV)
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,17,0)
+#include <asm/hyperv-tlfs.h>
+
+struct hv_vp_assist_page **hv_vp_assist_page;
+EXPORT_SYMBOL_GPL(hv_vp_assist_page);
+#endif
+#endif
--- x86/external-module-compat.h
+++ x86/external-module-compat.h
@@ -14,6 +14,7 @@
 #include "../external-module-compat-comm.h"
 
 #include <asm/msr.h>
+#include <asm/msr-index.h>
 #include <asm/asm.h>
 #include <asm/desc.h>
 
@@ -58,3 +59,66 @@
 #endif
 
 #include <linux/smp.h>
+
+#ifndef MSR_F15H_PERF_CTL
+#define MSR_F15H_PERF_CTL               0xc0010200
+#endif
+#ifndef MSR_F15H_PERF_CTL0
+#define MSR_F15H_PERF_CTL0              MSR_F15H_PERF_CTL
+#endif
+#ifndef MSR_F15H_PERF_CTL1
+#define MSR_F15H_PERF_CTL1              (MSR_F15H_PERF_CTL + 2)
+#endif
+#ifndef MSR_F15H_PERF_CTL2
+#define MSR_F15H_PERF_CTL2              (MSR_F15H_PERF_CTL + 4)
+#endif
+#ifndef MSR_F15H_PERF_CTL3
+#define MSR_F15H_PERF_CTL3              (MSR_F15H_PERF_CTL + 6)
+#endif
+#ifndef MSR_F15H_PERF_CTL4
+#define MSR_F15H_PERF_CTL4              (MSR_F15H_PERF_CTL + 8)
+#endif
+#ifndef MSR_F15H_PERF_CTL5
+#define MSR_F15H_PERF_CTL5              (MSR_F15H_PERF_CTL + 10)
+#endif
+
+#ifndef MSR_F15H_PERF_CTR
+#define MSR_F15H_PERF_CTR               0xc0010201
+#endif
+#ifndef MSR_F15H_PERF_CTR0
+#define MSR_F15H_PERF_CTR0              MSR_F15H_PERF_CTR
+#endif
+#ifndef MSR_F15H_PERF_CTR1
+#define MSR_F15H_PERF_CTR1              (MSR_F15H_PERF_CTR + 2)
+#endif
+#ifndef MSR_F15H_PERF_CTR2
+#define MSR_F15H_PERF_CTR2              (MSR_F15H_PERF_CTR + 4)
+#endif
+#ifndef MSR_F15H_PERF_CTR3
+#define MSR_F15H_PERF_CTR3              (MSR_F15H_PERF_CTR + 6)
+#endif
+#ifndef MSR_F15H_PERF_CTR4
+#define MSR_F15H_PERF_CTR4              (MSR_F15H_PERF_CTR + 8)
+#endif
+#ifndef MSR_F15H_PERF_CTR5
+#define MSR_F15H_PERF_CTR5              (MSR_F15H_PERF_CTR + 10)
+#endif
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,17,0)
+static inline unsigned long cpu_kernelmode_gs_base(int cpu)
+{
+	unsigned long a;
+	rdmsrl(MSR_GS_BASE, a);
+	return a;
+}
+
+static inline void save_fsgs_for_kvm(void)
+{
+        savesegment(fs, current->thread.fsindex);
+        savesegment(gs, current->thread.gsindex);
+        if (unlikely(current->thread.fsindex != 0))
+		current->thread.fsbase = 0;
+        if (unlikely(current->thread.gsindex != 0))
+		current->thread.gsbase = 0;
+}
+#endif
